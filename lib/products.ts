1d45ca8de29e08216d8a0e2802d41bb9be296332
komplikatedkoding <82335431+komplikatedkoding@users.noreply.github.com>
2022-04-17 03:51:51 -0700

    Update products.ts

--- lib/products.ts
+++ lib/products.ts
@@ -1,18 +1,18 @@
 export const products = [
   {
-    id: 'box-of-cookies',
-    name: 'Box',
+    id: 'bag-of-kitten',
+    name: 'Bag',
     description: 'A delicious box of handmade cookies',
-    unitName: 'box', // shows after the price, eg. 0.05 SOL/box
-    priceSol: 0.05,
-    priceUsd: 5,
+    unitName: 'bag', // shows after the price, eg. 0.05 SOL/box
+    priceSol: 0.8,
+    priceUsd: 80,
   },
   {
-    id: 'basket-of-cookies',
+    id: 'basket-of-kittens',
     name: 'Basket',
     description: 'A large basket of handmade cookies',
     unitName: 'basket',
-    priceSol: 0.1,
-    priceUsd: 10,
+    priceSol: 14,
+    priceUsd: 1400,
   }
 ]
